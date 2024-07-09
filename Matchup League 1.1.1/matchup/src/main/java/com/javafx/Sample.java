426935bbb170d6bd4285ae96e48caf6bb39d1099
Logan Nyquist <150978719+lognog2@users.noreply.github.com>
2024-07-08 20:48:43 -0400

    Delete Matchup League 1.1.1/matchup directory

--- Matchup League 1.1.1/matchup/src/main/java/com/javafx/Sample.java
+++
@@ -1,9 +0,0 @@
-package com.javafx;
-
-import java.io.IOException;
-import javafx.fxml.FXML;
-
-public class Sample extends MenuHandler {
-
-    @FXML private void toMenu() throws IOException {super.toMainMenu();}
-}
